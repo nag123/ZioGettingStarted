3bc461fdb87982dbc738558ccf918f19f55a2536
nagini s <nagini18@gmail.com>
2024-08-18 01:25:39 -0400

    Recap of Scala in UnderstandingBasics

---
+++ src/main/scala/com/letsExploreZio/UnderstandingBasics/Variance.scala
@@ -0,0 +1,13 @@
+package com.letsExploreZio.UnderstandingBasics
+
+object Variance {
+
+  // OOP -We rely on substitution , which rely on subtyping
+  class Animal
+  class Dog(name: String) extends Animal
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
